77e1f4399ad667d231a570e9e717d7a8394c713f
Janusz Jakubiec <janusz.jakubiec@gmail.com>
2024-02-26 15:35:51 +0100

    Adding n_queen problem

--- meow_runner/lib/n_queen.ex
+++ meow_runner/lib/n_queen.ex
@@ -7,24 +7,24 @@ defmodule NQueen do
     genomes =
       Nx.tensor([
         [
-          [1, 0, 0, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0, 1, 1],
           [0, 1, 0, 0, 0, 0, 0, 0],
           [0, 0, 1, 0, 0, 0, 0, 0],
           [0, 0, 0, 1, 0, 0, 0, 0],
           [0, 0, 0, 0, 1, 0, 0, 0],
           [0, 0, 0, 0, 0, 1, 0, 0],
-          [0, 0, 0, 0, 0, 0, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 1]
+          [1, 0, 0, 0, 0, 0, 1, 0],
+          [1, 1, 0, 0, 0, 0, 0, 1]
         ],
         [
-          [1, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 1, 0, 0, 0, 0, 1, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0, 1, 1],
+          [0, 1, 0, 0, 0, 0, 0, 1],
+          [0, 1, 0, 0, 0, 1, 1, 0],
           [0, 0, 0, 0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 1, 0]
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0],
+          [1, 0, 0, 0, 0, 0, 1, 0]
         ]
       ])
 
@@ -34,11 +34,81 @@ defmodule NQueen do
 
     genomes = genomes |> Nx.reshape({genomes_n, size, size})
 
-    # |> Nx.slice([0, 0, 0], [genomes_n, size, size * 2], strides: [1, 1, 1])
+    Nx.sum(genomes, axes: [1, 2])
+    |> Nx.subtract(count_columns(genomes))
+    |> Nx.subtract(count_rows(genomes))
+    |> Nx.subtract(count_diagonal(genomes))
+    |> Nx.subtract(count_diagonal(Nx.reverse(genomes, axes: [2])))
+  end
 
-    # t = Nx.iota({900})
-    # t = Nx.reshape(t, {2, 15, 30})
-    # Nx.slice(t, [0, 4, 11], [2, 3, 9], strides: [1, 1, 1])
-    Nx.take_diagonal(genomes[0])
+  defn count_columns(genomes) do
+    Nx.sum(genomes, axes: [1])
+    |> Nx.subtract(1)
+    |> Nx.max(0)
+    |> Nx.sum(axes: [1])
+  end
+
+  defn count_rows(genomes) do
+    Nx.sum(genomes, axes: [2])
+    |> Nx.subtract(1)
+    |> Nx.max(0)
+    |> Nx.sum(axes: [1])
+  end
+
+  defn count_diagonal(genomes) do
+    {genomes_n, size, _} = Nx.shape(genomes)
+
+    acc =
+      sum_genomes_diagonal(genomes, Nx.tensor(-size + 1))
+      |> Nx.broadcast({2 * size - 1, genomes_n})
+
+    {_, acc} =
+      while {x = -size + 2, acc}, Nx.less(x, size) do
+        sum =
+          sum_genomes_diagonal(genomes, x)
+          |> Nx.broadcast({2 * size - 1, genomes_n})
+
+        pred =
+          Nx.iota({2 * size - 1, 2}, axis: 0)
+
+        {x + 1, Nx.select(pred == x + size - 1, sum, acc)}
+      end
+
+    Nx.sum(acc, axes: [0])
+  end
+
+  defn sum_genomes_diagonal(genomes, offset) do
+    {genomes_n, data_size, _} = Nx.shape(genomes)
+    first_indice = Nx.iota({data_size, 1})
+    second_indice = Nx.iota({data_size, 1}) + offset
+
+    genomes_with_added_element =
+      Nx.concatenate([genomes, Nx.broadcast(0, {1, data_size, data_size})], axis: 0)
+
+    pred =
+      check_if_indices_are_proper(second_indice, data_size)
+      |> Nx.broadcast({genomes_n, data_size, 3})
+
+    indices =
+      Nx.concatenate([first_indice, second_indice], axis: 1)
+      |> Nx.broadcast({genomes_n, data_size, 2})
+
+    indices = Nx.concatenate([Nx.iota({genomes_n, data_size, 1}, axis: 0), indices], axis: 2)
+
+    selected_indices =
+      Nx.select(
+        pred,
+        indices,
+        Nx.broadcast(Nx.tensor([genomes_n, 0, 0]), {genomes_n, data_size, 3})
+      )
+
+    Nx.gather(genomes_with_added_element, selected_indices)
+    |> Nx.sum(axes: [1])
+    |> Nx.subtract(1)
+    |> Nx.max(0)
+  end
+
+  defn check_if_indices_are_proper(y_indice, size) do
+    y_indice >= 0 and y_indice < size
   end
 end
